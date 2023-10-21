deb2280752763503339d2a08a24ab0874134bb2e
[saitamakenmin] <[chsh22052@g.nihon-u.ac.jp]>
2023-10-21 15:31:08 +0900

    change2

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import Nabi from "./Nabi";
+
+export default function Header() {
+    return (
+      <header>
+          <h1>Birdwatching</h1>
+           <img src="dove.png" alt="a simple dove logo" />
+           <Nabi />
+      </header>
+    );
+  } 
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import Welcometext from "./Welcometext";
+import Image from "./Image";
+
+export default function Main(){
+ return(
+    <main>
+        <Welcometext />
+        <Image />
+    </main>
+ )    
+}
